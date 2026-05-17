adedf107c7765837ef29e8deba3f3c3f7d3f0a44
pinebarron <john.b.frum@gmail.com>
2026-05-17 04:09:18 -0700

    Fix: use ES module export for test

--- api/test.js
+++ api/test.js
@@ -1,8 +1,8 @@
-module.exports = (req, res) => {
+export default function handler(req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify({
     working: true,
     hasSupabaseUrl: !!process.env.SUPABASE_URL,
     hasDatabaseUrl: !!process.env.DATABASE_URL
   }));
-};
+}
