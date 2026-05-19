12b9c3e4c26ea798ea79d35c3d44fd50cf0b8ffc
pinebarron <john.b.frum@gmail.com>
2026-05-19 13:26:57 -0700

    Update color scheme: forest green on stormy grey

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: '#16a34a',
+  themeColor: '#2d5a3d',
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
